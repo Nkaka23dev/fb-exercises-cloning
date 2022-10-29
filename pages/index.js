b68c3f211e0df792bf09e99bc2c6f3e9d4ae0d44
Nkaka23dev <nkakaeric96@gmail.com>
2022-10-29 12:39:28 +0200

    added new branch

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex h-screen">
       <h1 className="text-3xl font-bold underline text-red-400 m-auto">
-      Comming soon.......
+       Dev branch is on the way...
     </h1>
     </div>
   )
